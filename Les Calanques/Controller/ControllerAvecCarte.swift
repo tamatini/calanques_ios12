d54457a99896599a9e964e00c61474b03dbeab7f
TEAHUI Tamatini <Tamatini@macbook-pro-de-tamatini.local>
2018-12-24 00:04:26 -1000

    ajout type de carte avec uiSegmentedControl

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -7,24 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class ControllerAvecCarte: UIViewController {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: mapView.mapType = MKMapType.standard
+        case 1: mapView.mapType = .satellite
+        case 2: mapView.mapType = .hybrid
+        default: break
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        }
     }
-    */
+    
+    @IBAction func getPosition(_ sender: Any) {
+    }
 
 }
