bdb9ff0c85b9a450bca144b604e5b412be903cc2
TEAHUI Tamatini <Tamatini@macbook-pro-de-tamatini.local>
2018-12-24 00:49:48 -1000

    ajout des contraintes annotationView

--- Les Calanques/Vue/monAnnotationView.swift
+++ Les Calanques/Vue/monAnnotationView.swift
@@ -32,18 +32,35 @@ class monAnnotationView: MKAnnotationView {
     func setupLeft() -> UIButton {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         button.setImage(UIImage(named: "distance"), for: .normal)
+        button.addTarget(self, action: #selector(gps), for: .touchUpInside)
         return button
     }
     
     func setupRight() -> UIButton {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         button.setImage(UIImage(named: "detail"), for: .normal)
+        button.addTarget(self, action: #selector(detail), for: .touchUpInside)
         return button
     }
     
-    func setupCenter() -> UIView {
+    func setupCenter() -> UIView? {
+        guard let anno = annotation as? MonAnnotation else { return nil}
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 125, height: 125))
-        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        
+        let imageView = UIImageView(frame: view.bounds)
+        imageView.image = anno.calanque.image
+        imageView.clipsToBounds = true
+        imageView.addSubview(imageView)
         return view
+    }
+    
+    @objc func detail() {
+    }
+    
+    @objc func gps() {
+        
     }
 }
